5134e6e525b5120e9df25e788ae0f5f0c7b46218
RobertSimms <Robert.simms@opencastsoftware.com>
2018-03-05 15:17:50 +0000

    Initial commit

---
+++ src/main/java/com/opencast/SpringSample/HelloWorld.java
@@ -0,0 +1,98 @@
+package com.opencast.SpringSample;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+
+    private String read;
+    private String write;
+    private String check;
+    private IFileWriter writer;
+
+    private static ArrayList<String> messageData;
+    private static ArrayList<String> messageread = new ArrayList<String>();
+
+
+
+    public void setRead(String read) {
+        this.read  = read;
+    }
+
+    public void setWrite(String write) {
+        this.write  = write;
+    }
+
+    public void setCheck(String check) {
+        this.check  = check;
+    }
+
+    public void setWriter(IFileWriter writer) {
+        this.writer = writer;
+    }
+
+    public IFileWriter getWriter(){
+        return writer;
+    }
+
+    public void getMessage() {
+        System.out.println("Your Message : " + read);
+    }
+
+
+    public void read() {
+        System.out.println("Read Start");
+
+        String FILENAME;
+        FILENAME = this.read;
+        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                messageread.add(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Read End");
+        messageData = messageread;
+    }
+
+    public void check() {
+
+        System.out.println("Check Start");
+        for(String item : messageData)
+        {
+            System.out.println(item);
+        }
+        System.out.println("Check End ");
+    }
+
+    public void write() throws IOException{
+        System.out.println("write Start");
+	   	createFile("output", messageData);
+	   	System.out.println("write End");
+    }
+
+    private void createFile(String file, ArrayList<String> arrData) throws IOException {
+        FileWriter writer = new FileWriter(file + ".txt");
+        int size = arrData.size();
+        for (int i=0;i<size;i++) {
+            String str = arrData.get(i).toString();
+            writer.write(str);
+            if(i < size-1) writer.write("\n");
+        }
+        writer.close();
+    }
+
+    public void run() {
+        this.writer.write();
+    }
+}
+
---
+++ src/main/java/com/opencast/SpringSample/IFileWriter.java
@@ -0,0 +1,6 @@
+package com.opencast.SpringSample;
+
+public interface IFileWriter {
+
+	void write();
+}
