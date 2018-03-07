bf4b1739a9b1d50947fbdc5054abec1843e96f98
Drew Miley <drew.miley@opencastsoftware.com>
2018-03-07 10:50:27 +0000

    App now runs and fails, updates dependencies

--- src/main/java/com/opencast/SpringSample/MainApp.java
+++ src/main/java/com/opencast/SpringSample/MainApp.java
@@ -1,10 +1,15 @@
 package com.opencast.SpringSample;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BeansException {
+        run();
+    }
+
+    public static void run() throws BeansException {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
     }
