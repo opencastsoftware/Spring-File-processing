db9885a52230f07ff12903f19cf17136a6dd24fb
RobertSimms <Robert.simms@opencastsoftware.com>
2018-03-09 16:03:13 +0000

    working update

--- src/main/java/uk/org/ppa/SpringSample/HelloWorld.java
+++ src/main/java/uk/org/ppa/SpringSample/HelloWorld.java
@@ -1,10 +1,15 @@
 package uk.org.ppa.SpringSample;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.*;
 import java.util.ArrayList;
 
+
 public class HelloWorld {
     private String location;
+    public static String fileName;
+    public static String fileLocation;
     private static IFileWriter writer;
 
     private static  ArrayList<String> messageData;
@@ -29,7 +34,7 @@ public class HelloWorld {
         return messageData;
     }
 
-    public  void read(File test) {
+    public  ArrayList<String> read(File test) {
         System.out.println("Read Start");
         ArrayList<String> messageread = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(test))) {
@@ -45,11 +50,12 @@ public class HelloWorld {
         }
         System.out.println("Read End");
         messageData = messageread;
+        return messageread;
     }
 
-    public void write() throws IOException{
+    public void write(ArrayList<String> test) throws IOException{
         System.out.println("write Start");
-	   	createFile(this.writer.getFilename(), this.writer.getInitialText(), this.writer.getDataToWrite());
+	   	createFile(fileName, "gdfgd", test);
 	   	System.out.println("write End");
     }
 
@@ -67,10 +73,24 @@ public class HelloWorld {
         }
         writer.close();
     }
+    public ArrayList<String> run(ArrayList<String>  test) throws IOException {
+        System.out.println("Run Start");
+        for (String item:
+             test) {
+            System.out.println(item);
 
-    public void run() throws IOException {
-        this.writer.setMessageData(this.getMessageData());
-        this.writer.processData();
+        }
+        if(this.writer == null) {
+            System.out.println("not there");
+        }
+        else
+        {
+            System.out.println("found");
+        }
+//        this.writer.setMessageData(test);
+//        this.writer.processData();
+        System.out.println("Run End");
+        return test;
     }
 
 
