37bb19b9b160adaa655f93fa945571e608e48e8c
Drew Miley <drew.miley@opencastsoftware.com>
2018-03-12 09:21:08 +0000

    Adds \r

--- src/main/java/uk/org/ppa/FlatFileProcessing/FlatFileWriter.java
+++ src/main/java/uk/org/ppa/FlatFileProcessing/FlatFileWriter.java
@@ -7,36 +7,30 @@ import java.util.ArrayList;
 
 public class FlatFileWriter {
 
-    private String location;
+    private String fileLocation;
 
-    private String fileName;
-
-    public void write(ArrayList<String> test) throws IOException {
+    public void write(ArrayList<String> dataToWrite) throws IOException {
         System.out.println("write Start");
-        createFile(fileName, location, test);
+        createFile(fileLocation, dataToWrite);
         System.out.println("write End");
     }
 
-    private void createFile(String fileName, String location, ArrayList<String> arrData) throws IOException {
-        final File file = new File(location, fileName);
+    private void createFile(String fileLocation, ArrayList<String> dataToWrite) throws IOException {
+        final File file = new File(fileLocation);
         final FileWriter writer = new FileWriter(file);
-        int size = arrData.size();
-        writer.write("\n");
+        int size = dataToWrite.size();
+        writer.write("\r\n");
         for (int i = 0; i < size; i++) {
-            String str = arrData.get(i);
+            String str = dataToWrite.get(i);
             writer.write(str);
             if (i < size-1) {
-                writer.write("\n");
+                writer.write("\r\n");
             }
         }
         writer.close();
     }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
+    public void setFileLocation(String fileLocation) {
+        this.fileLocation = fileLocation;
     }
 }
